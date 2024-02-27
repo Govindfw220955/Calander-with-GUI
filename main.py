ad16b59792de5b942907c38deaad6b2dc22a01cc
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2024-02-27 18:51:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Import all the required libraries
-import tkinter
 import tkinter as tk
+
 from tkcalendar import Calendar
-from PIL import Image, ImageTk
+
 
 # Create the Tkinter winder and insert the root file and create function
 # for current date information
@@ -16,10 +16,10 @@ def get_date():
 
 root = tk.Tk()
 root.title("First GUI Calendar")
-root.geometry("200x200")
+root.geometry("400x400")
 root.configure(background="green")
 sid = tk.Label(text="This is My first GUI Calendar", bg="dark gray", relief="sunken")
-sid.pack()
+sid.place(relx=0.5, rely=0, anchor="n")
 
 # Set the window size and create Calendar desk or menu.
 
@@ -31,8 +31,7 @@ cal.pack(pady=20,fill="both", expand=True)
 
 #  Create a button to trigger date selection and link it to the get_date() function
 select_button = tk.Button(root, text="Select Date", command=get_date,bg="red", relief="groove")
-select_button.place(x=180,y=190)
-select_button.pack(pady=10)
+select_button.place(relx=0.5, rely=0.9, anchor="s")
 
 # Create a label to display the selected date
 
